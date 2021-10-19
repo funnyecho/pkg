d38899a9b0ead53115346fcbb2482316b18f0bc1
SamHwang1990 <samhwang1990@gmail.com>
2021-10-19 14:29:42 +0800

    feat(logger): place stdout port transport in bubble stage

--- packages/logger/src/transports/stdout.ts
+++ packages/logger/src/transports/stdout.ts
@@ -3,24 +3,26 @@ import loggerLevel from '../level';
 import Field from '../field';
 
 function withStdout(): ITransport {
-  return (_, entry) => {
-    const { owner, level, message, fields } = entry;
-    const label = `${message} [${owner}]`
-    const fieldMap = Field.mapFieldList(fields);
-    const output = `${label} ${JSON.stringify(fieldMap, null, 2)}`;
+  return {
+    bubble: (_, entry) => {
+      const { owner, level, message, fields } = entry;
+      const label = `${message} [${owner}]`
+      const fieldMap = Field.mapFieldList(fields);
+      const output = `${label} ${JSON.stringify(fieldMap, null, 2)}`;
 
-    switch (level) {
-      case loggerLevel.LevelEnum.error:
-      case loggerLevel.LevelEnum.fatal:
-        console.error(output);
-        break;
-      case loggerLevel.LevelEnum.info:
-        console.info(output);
-        break;
-      default:
-      case loggerLevel.LevelEnum.debug:
-        console.debug(output);
-        break;
+      switch (level) {
+        case loggerLevel.LevelEnum.error:
+        case loggerLevel.LevelEnum.fatal:
+          console.error(output);
+          break;
+        case loggerLevel.LevelEnum.info:
+          console.info(output);
+          break;
+        default:
+        case loggerLevel.LevelEnum.debug:
+          console.debug(output);
+          break;
+      }
     }
   };
 }
